41ff50bb4bc5f462ee35c266979ae24b26583722
dougsn <douglassnascimet@gmail.com>
2022-04-12 20:07:02 -0300

    Criando o script tasks e realizando a requisição do getMe

---
+++ scripts/tarefas/tasks.js
@@ -0,0 +1,25 @@
+onload = () => {
+  let nomeUsuario = document.querySelector('.user-info p')
+  let endPointLogin = "https://ctd-todo-api.herokuapp.com/v1/users/getMe";
+
+  let tokenJwt = localStorage.getItem("jwt");
+
+  let configRequisicao = {
+    method: "GET",
+    headers: {
+      authorization: tokenJwt,
+    },
+  };
+  fetch(endPointLogin, configRequisicao)
+    .then((result) => {
+      if (result.status == 200) {
+        return result.json();
+      }
+    })
+    .then((result) => {
+        nomeUsuario.innerText = `Olá, ${result.firstName}`;
+    })
+    .catch((erro) => {
+        console.log(erro);
+    });
+};
