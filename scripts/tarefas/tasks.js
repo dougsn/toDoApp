1c9385cf7601ce97cebb104ba1b36a082516c794
Douglas da Silva Nascimento <dnascimento@enavirj.com.br>
2022-04-13 16:42:30 -0300

    Começando a inserir informações das tarefas pendentes

--- scripts/tarefas/tasks.js
+++ scripts/tarefas/tasks.js
@@ -38,6 +38,11 @@ onload = () => {
     })
     .then((result) => {
       console.log(result);
+      result.forEach(element => {
+        console.log(element);
+      tarefaPendente(element)
+      }) 
+
     })
     .catch((erro) => {
       console.log(erro);
@@ -53,10 +58,10 @@ onload = () => {
     const inputTarefa = document.getElementById("novaTarea").value;
 
     let bodyNewTask = {
-      "description": inputTarefa,
-      "completed":false
-    }
-    
+      description: inputTarefa,
+      completed: false,
+    };
+
     let newTaskJson = JSON.stringify(bodyNewTask); // Foi convertida para JSON para conseguirmos enviar para o servidor
 
     let configNewTasks = {
@@ -79,4 +84,24 @@ onload = () => {
         console.log(e);
       });
   });
+
+  function tarefaPendente(e) {
+ 
+      
+    let tarefasPendentesHtml = document.querySelector('.tarefas-pendentes')
+        console.log(e);
+        tarefasPendentesHtml.innerHTML = `
+        <li class="tarefa">
+            <div class="not-done" id="${e.id}"></div>
+            <div class="descricao">
+                <p class="nome">${e.description}</p>
+                <p class="timestamp"><i class="far fa-calendar-alt"></i> ${e.createdAt}</p>
+            </div>
+          </li>
+        `
+        
+      tarefasPendentesHtml.appendChild(pendente)
+
+
+  }
 };
